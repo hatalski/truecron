e536c226551e52f05d55ef4d76ee57b73199c51d
Dmitry Vorobyev <dmitry@vorobyev.name>
2014-08-26 15:34:38 +0400

    [*] interactive navigation sidebar

--- routes/index.js
+++ routes/index.js
@@ -7,8 +7,8 @@ router.get('/', function(req, res) {
 	res.render('index', { title: 'TrueCron' });
 });
 
-router.get('/hello', function(req, res) {
+/*router.get('/hello', function(req, res) {
 	res.render('hello', { title: 'Hello World' });
-});
+});*/
 
 module.exports = router;
