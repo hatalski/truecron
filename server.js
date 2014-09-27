83bc66c8ca9375d5f7b98a59e7edf0ab90c322a1
Vitali Hatalski <v.hatalski@gmail.com>
2014-09-27 19:09:42 +0300

    changed logger in server.js

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
-var debug = require('debug')('TrueCron');
+var log = require('./lib/logger');
 var app = require('./app');
 
 app.set('port', 80);
 
 var server = app.listen(app.get('port'), function() {
-  debug('Express server listening on port ' + server.address().port);
+  log.info('Express server listening on port ' + server.address().port);
 });
