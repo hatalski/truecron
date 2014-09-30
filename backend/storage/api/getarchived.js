a5ada14ce6d56e67a254f56588e415c585edbe1a
Andrei <andrew.kasatkin@perkslab.com>
2014-09-30 11:22:27 +0300

    filter archived

---
+++ backend/storage/api/getarchived.js
@@ -0,0 +1,21 @@
+/**
+ * Created by Andrew on 30.09.2014.
+ */
+var pg = require('pg');
+
+var conString = "postgres://vagrant:vagrant@192.168.3.20:5432/vagrant";
+
+var client = new pg.Client(conString);
+client.connect(function(err) {
+    if(err) {
+        return console.error('could not connect to postgres', err);
+    }
+    client.query('SELECT * FROM tc.job WHERE archived = 1', function(err, result) {
+        if(err) {
+            return console.error('error running query', err);
+        }
+        //return result;
+        console.log(result.rows[0]);
+        client.end();
+    });
+});
