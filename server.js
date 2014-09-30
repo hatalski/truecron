710f223649c228f180b3db3bc50d954a45959831
Vitali Hatalski <v.hatalski@gmail.com>
2014-09-30 07:05:24 -0400

    restore port 3000

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 #!/usr/bin/env node
 var log = require('./lib/logger');
 var app = require('./app');
+var config = require('./lib/config');
 
-app.set('port', 80);
+app.set('port', config.get('PORT'));
 
 var server = app.listen(app.get('port'), function() {
   log.info('Express server listening on port ' + server.address().port);
