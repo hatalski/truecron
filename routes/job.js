6f63ce5e159242707393487967a515f9adcb1e75
Andrei <andrew.kasatkin@perkslab.com>
2014-09-30 23:42:06 +0300

    filter tags, active and archive

--- routes/job.js
+++ routes/job.js
@@ -21,6 +21,7 @@ log.info(conString);
 router.get('/organizations/:org_id/workspaces/:workspace_id/jobs', function(req, res) {
     var organization_id = req.param('org_id');
     var workspace_id = req.param('workspace_id');
+    var tags_job = req.param('tags');
 
     debugger;
     var archived = req.param('archived', 0); // default: 0
@@ -34,19 +35,36 @@ router.get('/organizations/:org_id/workspaces/:workspace_id/jobs', function(req,
         if(err) {
             return console.error('could not connect to postgres', err);
         }
-        client.query('SELECT tc.Job.id, tc.Job.workspaceId, tc.Job.name, tc.Job.active, tc.Job.archived, tc.Job.createdAt, ' +
-                'tc.Job.updatedAt, tc.Job.updatedByPersonId, tc.Job.startsAt, tc.Job.rrule FROM tc.Job, ' +
-                'tc.Workspace WHERE active=$1 AND archived=$2 AND workspaceId=$3 AND organizationId=$4;',
-            [active, archived, workspace_id, organization_id],
-            function(err, result) {
-                if(err) {
-                    return console.error('error running query', err);
-                }
+        if(tags_job) {
+            client.query('SELECT tc.Job.id, tc.Job.workspaceId, tc.Job.name, tc.Job.active, tc.Job.archived, tc.Job.createdAt, ' +
+                    'tc.Job.updatedAt, tc.Job.updatedByPersonId, tc.Job.startsAt, tc.Job.rrule FROM tc.Job, ' +
+                    'tc.Workspace WHERE active=$1 AND archived=$2 AND workspaceId=$3 AND organizationId=$4;',
+                [active, archived, workspace_id, organization_id],
+                function (err, result) {
+                    if (err) {
+                        return console.error('error running query', err);
+                    }
 
-                res.json({ "jobs": result.rows });
+                    res.json({ "jobs": result.rows });
 
-                client.end();
-            });
+                    client.end();
+                });
+        }
+        else{
+            client.query('SELECT tc.Job.id, tc.Job.workspaceId, tc.Job.name, tc.Job.active, tc.Job.archived, tc.Job.createdAt, ' +
+                    'tc.Job.updatedAt, tc.Job.updatedByPersonId, tc.Job.startsAt, tc.Job.rrule FROM tc.Job, ' +
+                    'tc.Workspace, tc.JobTag WHERE active=$1 AND archived=$2 AND workspaceId=$3 AND organizationId=$4 AND tag=$5;',
+                [active, archived, workspace_id, organization_id, tags_job],
+                function (err, result) {
+                    if (err) {
+                        return console.error('error running query', err);
+                    }
+
+                    res.json({ "jobs": result.rows });
+
+                    client.end();
+                });
+        }
     });
 });
 
