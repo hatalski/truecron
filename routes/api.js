e2a321a046a52bfb438213995bf56dcd7e757be6
Andrei <andrew.kasatkin@perkslab.com>
2014-10-02 18:12:32 +0300

    this code works, but there is duplication.

--- routes/api.js
+++ routes/api.js
@@ -16,31 +16,62 @@ var conString = "postgres://" +
     ":" + config.get('POSTGRE_PORT')
     + "/" + config.get('POSTGRE_DATABASE');
 
+log.info(conString);
+
 router.get('/organizations/:org_id/workspaces/:workspace_id/jobs', function(req, res) {
     var organization_id = req.param('org_id');
     var workspace_id = req.param('workspace_id');
 
+
     debugger;
     var archived = req.param('archived', 0); // default: 0
     var active = req.param('active', 1); // default: 1
+    var tags_job = req.param('tags');
+    var sort = req.param('sort', 'name');
+    var direction = req.param('direction', 'asc');
 
     log.info('org id: ' + organization_id);
     log.info('workspace id: ' + workspace_id);
     log.info('archived: ' + archived);
+    log.info('active: ' + active);
+    log.info('tags: ' + tags_job);
+    log.info('sort: ' + sort);
+    log.info('direction: ' + direction);
 
     pg.connect(conString, function(err, client, done) {
         if(err) {
             return console.error('could not connect to postgres', err);
         }
-        client.query('SELECT * FROM tc.job WHERE archived = $1 and active = $2', [archived, active], function(err, result) {
-            if(err) {
-                return console.error('error running query', err);
-            }
+        if(tags_job) {
+            client.query('SELECT tc.Job.id, tc.Job.workspaceId, tc.Job.name, tc.Job.active, tc.Job.archived, tc.Job.createdAt, ' +
+                    'tc.Job.updatedAt, tc.Job.updatedByPersonId, tc.Job.startsAt, tc.Job.rrule FROM tc.Job, ' +
+                    'tc.Workspace WHERE active=$1 AND archived=$2 AND workspaceId=$3 AND organizationId=$4 ORDER BY '+sort+' '+direction+';',
+                [active, archived, workspace_id, organization_id],
+                function (err, result) {
+                    if (err) {
+                        return console.error('error running query', err);
+                    }
 
-            res.json({ "jobs": result.rows });
+                    res.json({ "jobs": result.rows });
 
-            client.end();
-        });
+                    client.end();
+                });
+        }
+        else{
+            client.query('SELECT tc.Job.id, tc.Job.workspaceId, tc.Job.name, tc.Job.active, tc.Job.archived, tc.Job.createdAt, ' +
+                    'tc.Job.updatedAt, tc.Job.updatedByPersonId, tc.Job.startsAt, tc.Job.rrule FROM tc.Job, ' +
+                    'tc.Workspace, tc.JobTag WHERE active=$1 AND archived=$2 AND workspaceId=$3 AND organizationId=$4 AND tag=$5 ORDER BY '+sort+' '+direction+';',
+                [active, archived, workspace_id, organization_id, tags_job],
+                function (err, result) {
+                    if (err) {
+                        return console.error('error running query', err);
+                    }
+
+                    res.json({ "jobs": result.rows });
+
+                    client.end();
+                });
+        }
     });
 });
 
