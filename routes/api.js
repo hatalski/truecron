50dd44874bf0cd6ece0fb746d84122834ced16f3
Vitali Hatalski <v.hatalski@gmail.com>
2014-10-06 17:46:51 -0400

    change jobs API url

--- routes/api.js
+++ routes/api.js
@@ -16,75 +16,73 @@ var conString = "postgres://" +
     ":" + config.get('POSTGRE_PORT')
     + "/" + config.get('POSTGRE_DATABASE');
 
-router.get('/organizations/:org_id/workspaces/:workspace_id/jobs', function(req, res) {
-    var organization_id = req.param('org_id');
-    var workspace_id = req.param('workspace_id');
+router.route('/workspaces/:org_id/:workspace_id/jobs')
+    .get(function(req, res) {
+        var organization_id = req.param('org_id');
+        var workspace_id = req.param('workspace_id');
 
+        debugger;
+        var archived = req.param('archived', 0); // default: 0
+        var active = req.param('active', 1); // default: 1
+        var tags_job = req.param('tags');
+        var sort = req.param('sort', 'name');
+        var direction = req.param('direction', 'asc');
+        var wherefordb;
 
-    debugger;
-    var archived = req.param('archived', 0); // default: 0
-    var active = req.param('active', 1); // default: 1
-    var tags_job = req.param('tags');
-    var sort = req.param('sort', 'name');
-    var direction = req.param('direction', 'asc');
-    var wherefordb;
-
-    if(!tags_job){
-        wherefordb='WHERE active='+active+' AND archived='+archived+' AND workspaceId='+workspace_id+' AND organizationId='+organization_id+' ORDER BY '+sort+' '+direction+';';
-    }
-    else {
-        wherefordb='WHERE active='+active+' AND archived='+archived+' AND workspaceId='+workspace_id+' AND organizationId='+organization_id+' AND tag='+tags_job+'  ORDER BY '+sort+' '+direction+';';
-    }
-
-    log.info('org id: ' + organization_id);
-    log.info('workspace id: ' + workspace_id);
-    log.info('archived: ' + archived);
-    log.info('active: ' + active);
-    log.info('tags: ' + tags_job);
-    log.info('sort: ' + sort);
-    log.info('direction: ' + direction);
-
-    pg.connect(conString, function(err, client, done) {
-        if(err) {
-            return console.error('could not connect to postgres', err);
+        if (!tags_job) {
+            wherefordb = 'WHERE active=' + active + ' AND archived=' + archived + ' AND workspaceId=' + workspace_id + ' AND organizationId=' + organization_id + ' ORDER BY ' + sort + ' ' + direction + ';';
         }
-        client.query('SELECT tc.Job.id, tc.Job.workspaceId, tc.Job.name, tc.Job.active, tc.Job.archived, tc.Job.createdAt, ' +
-                'tc.Job.updatedAt, tc.Job.updatedByPersonId, tc.Job.startsAt, tc.Job.rrule FROM tc.Job, ' +
-                'tc.Workspace '+wherefordb,
-            function (err, result) {
-                if (err) {
-                    return console.error('error running query', err);
-                }
-
-                res.json({ "jobs": result.rows });
-
-                client.end();
-            });
-
-    });
-});
-
-/*post*/
-router.post('/organizations/:org_id/workspaces/:workspace_id/jobs', function(req, res) {
-    var workspaceId, name, archived, updatedByPersonId, rrule;
-    workspace_id = req.param('workspace_id');
-
-    pg.connect(conString, function(err, client, done) {
-        if(err) {
-            return console.error('could not connect to postgres', err);
+        else {
+            wherefordb = 'WHERE active=' + active + ' AND archived=' + archived + ' AND workspaceId=' + workspace_id + ' AND organizationId=' + organization_id + ' AND tag=' + tags_job + '  ORDER BY ' + sort + ' ' + direction + ';';
         }
-        client.query('insert into tc.Job (workspaceId, name, archived, updatedByPersonId, rrule) values ($1, $2, $3, $4, $5);', [workspaceId, name, archived, updatedByPersonId, rrule], /*add parse json*/
-            function (err, result) {
-                if (err) {
-                    return console.error('error running query', err);
-                }
 
-                req.json(result);
+        log.info('org id: ' + organization_id);
+        log.info('workspace id: ' + workspace_id);
+        log.info('archived: ' + archived);
+        log.info('active: ' + active);
+        log.info('tags: ' + tags_job);
+        log.info('sort: ' + sort);
+        log.info('direction: ' + direction);
 
-                client.end();
-            });
+        pg.connect(conString, function (err, client, done) {
+            if (err) {
+                return console.error('could not connect to postgres', err);
+            }
+            client.query('SELECT tc.Job.id, tc.Job.workspaceId, tc.Job.name, tc.Job.active, tc.Job.archived, tc.Job.createdAt, ' +
+                    'tc.Job.updatedAt, tc.Job.updatedByPersonId, tc.Job.startsAt, tc.Job.rrule FROM tc.Job, ' +
+                    'tc.Workspace ' + wherefordb,
+                function (err, result) {
+                    if (err) {
+                        return console.error('error running query', err);
+                    }
+
+                    res.json({ "jobs": result.rows });
+
+                    client.end();
+                });
+
+        });
+    })
+    .post(function(req, res) {
+        var workspaceId, name, archived, updatedByPersonId, rrule;
+        workspace_id = req.param('workspace_id');
+
+        pg.connect(conString, function(err, client, done) {
+            if(err) {
+                return console.error('could not connect to postgres', err);
+            }
+            client.query('insert into tc.Job (workspaceId, name, archived, updatedByPersonId, rrule) values ($1, $2, $3, $4, $5);', [workspaceId, name, archived, updatedByPersonId, rrule], /*add parse json*/
+                function (err, result) {
+                    if (err) {
+                        return console.error('error running query', err);
+                    }
+
+                    res.json(result);
+
+                    client.end();
+                });
+        });
     });
-});
 
 module.exports = router;
 
