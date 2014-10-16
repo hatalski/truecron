99b4d506abb0dba9b09e683722ac2f6108874092
Vitali Hatalski <v.hatalski@gmail.com>
2014-10-16 15:37:00 +0300

    create user test

--- test/index.js
+++ test/index.js
@@ -8,6 +8,20 @@ log.info('API tests prefix: ' + prefix);
 
 describe('USERS API',
     function() {
+        it('create a new user', function (done) {
+            superagent.post(prefix + '/users')
+                .set('Content-Type', 'application/json')
+                .send({"name": "Alice", "password": "P@ssw0rd"})
+                .auth('-2', 'Igd7en1_VCMP59pBpmEF')
+                .end(function (e, res) {
+                    expect(e).to.eql(null);
+                    console.dir(res.body);
+                    expect(res.header['content-type']).to.eql('application/json; charset=utf-8');
+                    expect(res.body.error).to.eql(null);
+                    expect(res.status).to.eql(201);
+                    done();
+                });
+        });
         it('get user by id', function (done) {
             superagent.get(prefix + '/users/1')
                 .send()
@@ -15,8 +29,10 @@ describe('USERS API',
                 .end(function (e, res) {
                     expect(e).to.eql(null);
                     expect(res.header['content-type']).to.eql('application/json; charset=utf-8');
+                    expect(res.body.error).to.eql(null);
                     expect(res.status).to.eql(200);
                     done();
                 });
         });
-    });
+    }
+);
