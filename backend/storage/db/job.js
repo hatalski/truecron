89272fcccba83b311b87a95ec9e8994eee49001f
Andrei <andrew.kasatkin@perkslab.com>
2014-10-16 12:36:08 +0300

    new in job.js

--- backend/storage/db/job.js
+++ backend/storage/db/job.js
@@ -2,12 +2,13 @@ module.exports = function(sequelize, DataTypes) {
 
     var Job = sequelize.define('Job', {
         id:                { type: DataTypes.BIGINT, primaryKey: true, allowNull: false, autoIncrement: true },
+        workspaceId:       { type: DataTypes.BIGINT},
         name:              { type: DataTypes.STRING(255), allowNull: false },
-        active:            { type: DataTypes.BOOLEAN, allowNull: false, defaultValue: false },
-        archived:          { type: DataTypes.BOOLEAN, allowNull: false, defaultValue: false },
-        rrule:             { type: DataTypes.TEXT, allowNull: false },
+        active:            { type: DataTypes.BOOLEAN, allowNull: false, defaultValue: 0 },
+        archived:          { type: DataTypes.BOOLEAN, allowNull: false, defaultValue: 0 },
+        updatedByPersonId: { type: DataTypes.BIGINT },
         startsAt:          { type: DataTypes.DATE },
-        updatedByPersonId: { type: DataTypes.BIGINT }
+        rrule:             { type: DataTypes.TEXT, allowNull: false }
     }, {
         schema: 'tc',
         tableName: 'job',
@@ -16,6 +17,7 @@ module.exports = function(sequelize, DataTypes) {
         classMethods: {
             associate: function(models) {
                 Job.belongsTo(models.Workspace, { as: 'workspace', foreignKey: 'workspaceId' });
+                Job.belongsTo(models.Person, { as: 'updatedBy', foreignKey: 'updatedByPersonId' });
             }
         }
     });
