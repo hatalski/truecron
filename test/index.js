0acff6d22cee6df66599df7d62165600fad8ffdf
Vitali Hatalski <v.hatalski@gmail.com>
2014-10-17 10:48:22 +0300

    uncomment user create and get list of users API tests

--- test/index.js
+++ test/index.js
@@ -8,20 +8,20 @@ log.info('API tests prefix: ' + prefix);
 
 describe('USERS API',
     function() {
-//        it('create a new user', function (done) {
-//            superagent.post(prefix + '/users')
-//                .set('Content-Type', 'application/json')
-//                .send({ 'user': {'name': "Alice", 'password': "P@ssw0rd"} })
-//                .auth('-2', 'Igd7en1_VCMP59pBpmEF')
-//                .end(function (e, res) {
-//                    expect(e).to.eql(null);
-//                    console.dir(res.body);
-//                    expect(res.header['content-type']).to.eql('application/json; charset=utf-8');
-//                    expect(res.body.error).to.eql(undefined);
-//                    expect(res.status).to.eql(201);
-//                    done();
-//                });
-//        });
+        it('create a new user', function (done) {
+            superagent.post(prefix + '/users')
+                .set('Content-Type', 'application/json')
+                .send({ 'user': {'name': "Alice", 'password': "P@ssw0rd"} })
+                .auth('-2', 'Igd7en1_VCMP59pBpmEF')
+                .end(function (e, res) {
+                    expect(e).to.eql(null);
+                    console.dir(res.body);
+                    expect(res.header['content-type']).to.eql('application/json; charset=utf-8');
+                    expect(res.body.error).to.eql(undefined);
+                    expect(res.status).to.eql(201);
+                    done();
+                });
+        });
         it('get list of users', function (done) {
             superagent.get(prefix + '/users')
                 .send()
@@ -34,17 +34,17 @@ describe('USERS API',
                     done();
                 });
         });
-//        it('get user by id', function (done) {
-//            superagent.get(prefix + '/users/1')
-//                .send()
-//                .auth('-2', 'Igd7en1_VCMP59pBpmEF')
-//                .end(function (e, res) {
-//                    expect(e).to.eql(null);
-//                    expect(res.header['content-type']).to.eql('application/json; charset=utf-8');
-//                    expect(res.body.error).to.eql(undefined);
-//                    expect(res.status).to.eql(200);
-//                    done();
-//                });
-//        });
+        it('get user by id', function (done) {
+            superagent.get(prefix + '/users/1')
+                .send()
+                .auth('-2', 'Igd7en1_VCMP59pBpmEF')
+                .end(function (e, res) {
+                    expect(e).to.eql(null);
+                    expect(res.header['content-type']).to.eql('application/json; charset=utf-8');
+                    expect(res.body.error).to.eql(undefined);
+                    expect(res.status).to.eql(200);
+                    done();
+                });
+        });
     }
 );
