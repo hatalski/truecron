c6533146b242eaa962a20e714b33c88bfdb3878b
Andrei <andrew.kasatkin@perkslab.com>
2014-10-17 14:14:16 +0300

    add post

--- backend/api/jobs.js
+++ backend/api/jobs.js
@@ -8,6 +8,21 @@ var express = require('express'),
 
 var api = express.Router();
 
+function jobAddLink(job) {
+    if (job === undefined) {
+        return job;
+    }
+    var jobs = job.toJSON();
+    var selfUrl = '/jobs/' + jobs.id;
+    jobs._links = {
+        self: selfUrl,
+        tasks: selfUrl + '/tasks',
+        history: selfUrl + '/history',
+        results: selfUrl + '/results'
+    };
+    return { jobs: jobs };
+}
+
 
 api.route('/jobs')
     //
@@ -33,5 +48,19 @@ api.route('/jobs')
                 }});
         });
     })
+
+    .post(function (req, res, next) {
+        if (!req.body || !req.body.user) {
+            return next(new apiErrors.InvalidParams());
+        }
+        storage.Person.create(req.body.user)
+            .then(function (job) {
+                res.status(201).json(jobAddLink(job));
+            })
+            .catch(function (err) {
+                logger.error(err);
+                return next(new apiErrors.InvalidParams(err));
+            });
+    });
 
 module.exports = api;
