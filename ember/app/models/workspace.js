cd73cdbb3d67b18115d34caba12228dd731e9890
Vitali Hatalski <v.hatalski@gmail.com>
2014-10-22 22:09:29 +0300

    + workspace relationship

--- ember/app/models/workspace.js
+++ ember/app/models/workspace.js
@@ -3,8 +3,8 @@ import DS from 'ember-data';
 export default DS.Model.extend({
     name:         DS.attr('string'),
     createdAt:    DS.attr('date'),
-    updatedAt:    DS.attr('date')//,
-    //updatedBy:    DS.belongsTo('person', { async: true }),
-    //jobs:         DS.hasMany('job', { async: true }),
-    //organization: DS.belongsTo('organization', { async: true })
+    updatedAt:    DS.attr('date'),
+    updatedBy:    DS.belongsTo('person', { async: true }),
+    jobs:         DS.hasMany('job', { async: true }),
+    organization: DS.belongsTo('organization', { async: true })
 });
--- ember/tests/unit/models/workspace-test.js
+++ ember/tests/unit/models/workspace-test.js
@@ -1,3 +1,4 @@
+import Ember from "ember";
 import {
   moduleForModel,
   test
@@ -5,11 +6,32 @@ import {
 
 moduleForModel('workspace', 'Workspace', {
   // Specify the other units that are required for this test.
-  needs: []
+  needs: ['model:job', 'model:person', 'model:organization', 'model:job-tag', 'model:task']
 });
 
 test('it exists', function() {
   var model = this.subject();
   // var store = this.store();
   ok(!!model);
+});
+
+test('has many jobs relationship', function() {
+    var Workspace = this.store().modelFor('workspace');
+    var relationship = Ember.get(Workspace, 'relationshipsByName').get('jobs');
+    equal(relationship.key, 'jobs');
+    equal(relationship.kind, 'hasMany');
+});
+
+test('belongs to person relationship', function() {
+    var Workspace = this.store().modelFor('workspace');
+    var relationship = Ember.get(Workspace, 'relationshipsByName').get('updatedBy');
+    equal(relationship.key, 'updatedBy');
+    equal(relationship.kind, 'belongsTo');
+});
+
+test('belongs to organization relationship', function() {
+    var Workspace = this.store().modelFor('workspace');
+    var relationship = Ember.get(Workspace, 'relationshipsByName').get('organization');
+    equal(relationship.key, 'organization');
+    equal(relationship.kind, 'belongsTo');
 });
