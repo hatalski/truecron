35ca310b58d4171c942d25358a6a689099485ff5
Andrei <andrew.kasatkin@perkslab.com>
2014-10-22 13:41:47 +0300

    get all jobs

--- backend/api/api.js
+++ backend/api/api.js
@@ -25,6 +25,7 @@ api.use(authenticate);
 // Routes
 //
 api.use(require('./users'));
+api.use(require('./jobsapi'));
 //api.use(require('./organizations'));
 //api.use(require('./workspaces'));
 
---
+++ backend/api/jobsapi.js
@@ -0,0 +1,53 @@
+/**
+ * Created by Andrew on 22.10.2014.
+ */
+
+ var express = require('express'),
+    _ = require('lodash'),
+    logger = require('../../lib/logger'),
+    validator = require('../../lib/validator'),
+    storage = require('../storage'),
+    apiErrors = require('./../../lib/errors'),
+    common = require('./common');
+
+var api = express.Router();
+
+function addLinks(datajob) {
+    if (datajob === undefined) {
+        return datajob;
+    }
+    var job = datajob.toJSON();
+    var selfUrl = '/jobs/' + job.id;
+    user._links = {
+        self: selfUrl        
+    };    
+    return { job: job };
+}
+
+api.route('/jobs')
+    //
+    // List of jobs
+    //
+    .get(common.parseListParams, function (req, res, next) {
+
+        var where = {};
+        if (!!req.listParams.searchTerm) {
+            where = { name: { like: req.listParams.searchTerm } };
+        }
+        var sort = req.listParams.sort || 'name';
+
+        storage.Jobs.findAndCountAll({
+            where: where,
+            order: sort + ' ' + req.listParams.direction,
+            limit: req.listParams.limit,
+            offset: req.listParams.offset
+        }).then(function (result) {
+                res.json({
+                jobs: result.rows.map(addLinks),
+                meta: {
+                    total: result.count
+                }});
+        });
+    })
+	
+	module.exports = api;
