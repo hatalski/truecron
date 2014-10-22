c33744a9a43666757d5886c13f7ae58ce83956ca
paul-sh <paul.shmakov@gmail.com>
2014-10-22 15:46:12 +0400

    Implemented a simple OAuth2 server at /oauth/token url. Supports 'password', 'client_credentials', and 'http://google' grant types (the last one is our extension, similar to 'password', but does not validate passwords. Just looks up a user by email). The API switched to this new authentication.

---
+++ backend/oauth/oautherrors.js
@@ -0,0 +1,51 @@
+var util = require('util');
+
+var OAuthError = module.exports.OAuthError = function (status, code, description) {
+    Error.call(this);
+    this.status = status || 'invalid_request';
+    this.code = code || 400;
+    this.description = description || '';
+    this.headers = null;
+};
+
+util.inherits(OAuthError, Error);
+
+module.exports.getInvalidRequest = function (description) {
+    return new OAuthError('invalid_request', 400, description);
+};
+
+module.exports.getInvalidClient = function (description) {
+    var err = new OAuthError('invalid_client', 401, description);
+    err.headers = { 'WWW-Authenticate': 'Basic realm="truecron"' };
+    return err;
+};
+
+module.exports.getInvalidGrant = function (description) {
+    return new OAuthError('invalid_grant', 400, description);
+};
+
+module.exports.getUnauthorizedClient = function (description) {
+    return new OAuthError('unauthorized_client', 400, description);
+};
+
+module.exports.getUnsupportedGrantType = function (description) {
+    return new OAuthError('unsupported_grant_type', 400, description);
+};
+
+module.exports.getInvalidScope = function (description) {
+    return new OAuthError('invalid_scope', 400, description);
+};
+
+module.exports.errorHandler = function (err, req, res, next) {
+    if (!(err instanceof OAuthError)) {
+        err = module.exports.getInvalidRequest(err.message);
+    }
+    if (!!err.headers) {
+        res.set(err.headers);
+    }
+    res.status(err.status || 400);
+    res.json({
+        error: err.status,
+        error_description: err.description
+    });
+};
---
+++ backend/oauth/server.js
@@ -0,0 +1,121 @@
+//
+// OAuth2 mini server
+//
+var express = require('express'),
+    Promise = require("bluebird"),
+    config = require('../../lib/config'),
+    validator = require('../../lib/validator'),
+    apiErrors = require('../../lib/errors'),
+    logger = require('../../lib/logger'),
+    secrets = require('../../lib/secrets'),
+    storage = require('../storage'),
+    clientAuth = require('./clientauth'),
+    oauthErrors = require('./oautherrors'),
+    token = require('./token');
+
+var router = express.Router();
+
+//
+// Client authentication, required for all requests
+//
+router.use(clientAuth);
+
+var supportedGrantTypes = ['password', 'client_credentials', 'http://google.com'];
+
+router.post('/token', function (req, res, next) {
+    if (!req.is('application/x-www-form-urlencoded')) {
+        return next(oauthErrors.getInvalidRequest('Request must use application/x-www-form-urlencoded encoding.'));
+    }
+    if (!req.body || !validator.isIn(req.body.grant_type, supportedGrantTypes)) {
+        return next(oauthErrors.getUnsupportedGrantType());
+    }
+
+    if (req.body.grant_type === 'password') {
+        return passwordAuth(req, res, next);
+    } else if (req.body.grant_type === 'client_credentials') {
+        return clientCredentialsAuth(req, res, next);
+    } else if (req.body.grant_type === 'http://google.com') {
+        return googleAuth(req, res, next); // Our grant extension, authenticates by just an email
+    } else {
+        next(oauthErrors.getUnsupportedGrantType())
+    }
+});
+
+var ensureClientCanAuthenticate = Promise.method(function(clientId, personId, grantType) {
+    // TODO: Consult the storage to figure out if the client is allowed to authenticate the given person.
+    if (clientId == -2) {
+        return true;
+    }
+    throw oauthErrors.getUnauthorizedClient();
+});
+
+var passwordAuth = function(req, res, next) {
+    req.checkBody('username', 'Missing username.').isLength(1, 256).isEmail();
+    req.checkBody('password', 'Missing password.').isLength(1);
+    var errors = req.validationErrors();
+    if (errors) {
+        return next(oauthErrors.getInvalidRequest());
+    }
+    return storage.Person.findByEmail(req.body.username)
+        .then(function (person) {
+            if (person === null) {
+                logger.debug('Failed to authenticate %s with password. User not found', req.body.username);
+                return next(oauthErrors.getInvalidGrant()); // Can't be more specific, otherwise we will reveal user emails
+            }
+
+            return ensureClientCanAuthenticate(req.client.id, person.id, 'password')
+                .then(function() {
+                    return secrets.comparePasswordAndHash(req.body.password, person.passwordHash)
+                })
+                .then(function (passwordIsGood) {
+                    if (passwordIsGood) {
+                        return token.issue(person.id, req, res, next);
+                    } else {
+                        logger.debug('Failed to authenticate %s with password. Invalid password.', req.body.username);
+                        next(oauthErrors.getInvalidGrant());
+                    }
+                });
+        })
+        .catch(function (err) {
+            logger.error('Failed to authenticate %s with password. %s', req.body.username, err.toString());
+            next(err);
+        });
+};
+
+/**
+ * This our grant extension for OAuth. Similar to 'password', but authenticates by just a email.
+ */
+var googleAuth = function(req, res, next) {
+    req.checkBody('username', 'Missing username.').isLength(1, 256).isEmail();
+    var errors = req.validationErrors();
+    if (errors) {
+        return next(oauthErrors.getInvalidRequest());
+    }
+    return storage.Person.findByEmail(req.body.username)
+        .then(function (person) {
+            if (person === null) {
+                logger.debug('Failed to authenticate %s with Google. User not found', req.body.username);
+                return next(oauthErrors.getInvalidGrant()); // Can't be more specific, otherwise we will reveal user emails
+            }
+
+            return ensureClientCanAuthenticate(req.client.id, person.id, 'password')
+                .then(function () {
+                    token.issue(person.id, req, res, next);
+                });
+        })
+        .catch(function (err) {
+            logger.error('Failed to authenticate %s with Google. %s', req.body.username, err.toString());
+            next(err);
+        });
+};
+
+var clientCredentialsAuth = function(req, res, next) {
+    // TODO: Think of the client authentication token. Does it need a user ID? Which one?
+    token.issue(-1, req, res, next);
+};
+
+//
+// Converts and returns errors according to OAUTH spec
+router.use(oauthErrors.errorHandler);
+
+module.exports = router;
