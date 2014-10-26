d8fe0547f1015a08e5e390b474180f9ca3b1d984
Vitali Hatalski <v.hatalski@gmail.com>
2014-10-27 00:44:24 +0300

    dashboard page - first step

--- ember/app/controllers/navigation.js
+++ ember/app/controllers/navigation.js
@@ -2,8 +2,10 @@ import Ember from 'ember';
 
 export default Ember.ArrayController.extend({
   organizations: Ember.A([
-    Ember.Object.create({title: "About", location: 'about', active: null}),
-    Ember.Object.create({title: "Projects", location: 'projects', active: null})
+    Ember.Object.create({name: "About", lastRun: '07/07/14 11:02 pm', nextRun: '07/07/14 11:02 pm', active: true, selected: true}),
+    Ember.Object.create({name: "Projects", lastRun: '07/07/14 11:02 pm', nextRun: '07/07/14 11:02 pm', active: false, selected: false})
   ]),
+  jobs: {
+  },
   title: "TrueCron"
 });
--- ember/app/router.js
+++ ember/app/router.js
@@ -6,8 +6,17 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-    this.route('jobs', { path: '/jobs'});
-  this.route('organizations');
+  this.route('jobs', { path: '/j'});
+  this.route('organizations', { path: '/o'}, function() {
+    this.route('organization', { path: '/:org_id'}, function() {
+      this.route('workspaces');
+      this.route('workspace', { path: '/:workspace_id'}, function() {
+        this.route('tasks', { path: '/tasks'}, function() {
+          this.route('task', { path: '/:task_id'});
+        });
+      });
+    });
+  });
 });
 
 export default Router;
