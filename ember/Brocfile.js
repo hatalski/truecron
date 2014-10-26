2042b4127fb2244e895debd17deeec11353db7a8
Vitali Hatalski <v.hatalski@gmail.com>
2014-10-27 00:15:38 +0300

    enable font-awesome

--- ember/Brocfile.js
+++ ember/Brocfile.js
@@ -13,7 +13,12 @@ var app = new EmberApp({
 // css
 app.import('bower_components/bootstrap/dist/css/bootstrap-theme.css');
 app.import('bower_components/bootstrap/dist/css/bootstrap.css');
-app.import('bower_components/bootstrap/dist/css/bootstrap-theme.css');
+app.import('bower_components/font-awesome/css/font-awesome.min.css');
+app.import("bower_components/font-awesome/fonts/fontawesome-webfont.eot", { destDir: "fonts" });
+app.import("bower_components/font-awesome/fonts/fontawesome-webfont.svg", { destDir: "fonts" });
+app.import("bower_components/font-awesome/fonts/fontawesome-webfont.ttf", { destDir: "fonts" });
+app.import("bower_components/font-awesome/fonts/fontawesome-webfont.woff", { destDir: "fonts" });
+app.import("bower_components/font-awesome/fonts/FontAwesome.otf", { destDir: "fonts" });
 
 // js
 app.import('bower_components/jquery/dist/jquery.js');
