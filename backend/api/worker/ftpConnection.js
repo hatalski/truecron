568905d55d7f947d14bf323df391e0d40f3db67f
estet <estetik.bel@gmail.com>
2014-10-30 14:08:04 +0300

    Fixed email test Added ftptask with test

--- backend/api/worker/ftpConnection.js
+++ backend/api/worker/ftpConnection.js
@@ -1,13 +1,18 @@
 /**
  * Created by estet on 10/26/14.
  */
-module.exports = function(host, username, password, protocol, port)
+module.exports = function(host, username, password, port, protocol)
 {
     this.host = host;
-    this.username = username;
-    this.password = password;
+    this.user = username;
+    this.pass = password;
     this.protocol = protocol ? protocol:'ftp';
     if(port) {
         this.port = port;
     }
+    else
+    {
+        this.port = 21;
+    }
+    this.debugMode = true;
 };
--- backend/api/worker/ftptask.js
+++ backend/api/worker/ftptask.js
@@ -4,36 +4,70 @@
 var task = require('./task'),
     transporter = require('../../../lib/smtp'),
     util = require('util'),
-    logger = require('../../../lib/logger'),
-    FTPS = require('ftps');
+    logger = require('../../../lib/logger');
+var JSFtp = require("jsftp");
 
-var ftpTask = function(ftpConnection, ftpTasks)
+var ftpTask = function(ftpConnection, ftpCommands)
 {
     var self = this;
     self.ftpConnection = ftpConnection;
     self.settings =
     {
-        ftpConnection : ftpConnection,
-        ftpTasks : ftpTasks
+        ftpConnection :ftpConnection,
+        ftpTasks : ftpCommands
     };
 
-    self.ftps = new FTPS(self.settings.ftpConnection);
+    self.ftpClient = new JSFtp(ftpConnection);
 
-    self.run = function()
+    self.run = function(callback)
     {
         if(!self.active) {
-            self.ftps.exec(ftpTasks, function(error, data)
+            self.start();
+            var quitFtp = function(callback)
             {
-                if(error)
-                {
-                    logger.error('Sorry ' + error + ' Data:' + JSON.stringify(data));
+                self.ftpClient.raw('QUIT', function (err, res) {
+                    self.afterSend(callback);
+                });
+            };
+
+            var callFtp = function(index)
+            {
+                var command = ftpCommands[index];
+                if(command) {
+                    if(command) {
+                        if(Object.prototype.toString.call( command ) !== '[object Array]')
+                        {
+                            command = [command, null];
+                        }
+                        try {
+                            self.ftpClient[command[0].toLowerCase()](command[1], function (err, res) {
+                                self.onError(err);
+                                self.onMessage(res);
+                                index++;
+                                callFtp(index);
+                            });
+                        }
+                        catch(ex)
+                        {
+                            self.onError(ex);
+                            quitFtp(callback);
+                        }
+                    }
                 }
                 else
                 {
-                    logger.info('Wow, FTP response: ' + data);
+                    quitFtp(callback);
                 }
-            });
+            };
+            callFtp(0);
         }
+    };
+
+    self.afterSend = function(callback)
+    {
+        if (callback && typeof callback === 'function')
+            callback();
+        this.complete();
     };
 
     self.init();
