015d8b85c59464f9e056fba415c5cf57bc43bc63
Vitali Hatalski <v.hatalski@gmail.com>
2014-11-09 23:08:30 +0300

    root route model is person

--- ember/app/routes/dashboard.js
+++ ember/app/routes/dashboard.js
@@ -2,14 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 	model: function() {
-		return this.store.find('organization');
-	},
-	afterModel: function(organizations, transition) {
-		console.log(organizations);
-		console.log(transition);
-		console.log(organizations.get('length'));
-	    if (organizations.get('length') >= 1) {
-	      this.transitionTo('dashboard.organization.workspace', organizations.get('firstObject'));
-	    }
+		// TODO: replace 1 with authenticated user id
+		return this.store.find('person', 1);
 	}
 });
