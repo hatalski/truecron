3430818206ab044fcd543afb1d04e458c0c3ecc7
estet <estetik.bel@gmail.com>
2014-11-18 23:34:41 +0300

    Updated exports for jobRunner

--- backend/api/worker/jobRunner.js
+++ backend/api/worker/jobRunner.js
@@ -2,39 +2,29 @@
  * Created by estet on 10/26/14.
  */
 
-var jobRunner = new function()
-{
-    this.runTasks = function(job, callBack)
-    {
-        if(!job)
-        {
-            throw new Exception('Sorry mate, but job is required parameter');
+var jobRunner = function(job, callBack) {
+    if (!job) {
+        throw new Exception('Sorry mate, but job is required parameter');
+    }
+
+    var executeTask = function (index) {
+        if (job.tasks.length > index) {
+            var task = job.tasks[index];
+            if (task) {
+                task.run(executeTask(index + 1));//on call back running next task in order
+            }
         }
-
-        var executeTask = function(index)
-        {
-            if(job.tasks.length > index)
-            {
-                var task = job.tasks[index];
-                if(task)
-                {
-                    task.run(executeTask(index + 1));//on call back running next task in order
-                }
+        else {
+            if (typeof callBack === 'function') {
+                callBack();
             }
-            else
-            {
-                if(typeof callBack === 'function')
-                {
-                    callBack();
-                }
-            }
-        };
-
-        if(job.tasks) {
-
-            executeTask(0);//Executing first task.
         }
     };
+
+    if (job.tasks) {
+
+        executeTask(0);//Executing first task.
+    }
 };
 
 module.exports = jobRunner;
