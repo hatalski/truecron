f2f90ec452381427a4cc62cb65be4cd006afe7d4
Vitali Hatalski <v.hatalski@gmail.com>
2014-11-20 14:18:49 +0300

    load tasks on row select

--- ember/app/controllers/dashboard/organization/workspace/tasks.js
+++ ember/app/controllers/dashboard/organization/workspace/tasks.js
@@ -4,4 +4,11 @@ export default Ember.ObjectController.extend({
 	// sortProperties: ['position'],
  //    sortAscending: true,
  //    itemController: 'dashboard.organization.workspace.tasks.task'
+ 	actions: {
+        viewtask: function(task) {
+            console.dir('task : ' + task);
+            console.dir('job : ' + task.get('job.id'));
+            this.transitionToRoute('dashboard.organization.workspace.tasks.task', task.get('job.id'), task);
+        }
+    }
 });
--- ember/app/routes/dashboard/organization/workspace/tasks/task.js
+++ ember/app/routes/dashboard/organization/workspace/tasks/task.js
@@ -3,10 +3,10 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function(params) {
   	console.log('load task with id : ' + params.task_id);
-    return this.store.find('task', params.task_id);
-  }//,
-  // serialize: function(model) {
-  // 	console.dir(model.get('id'));
-  //   return { task_id: model.get('id'), job_id: model.job.id };
-  // }
+  	return this.store.find('task', params.task_id);
+  },
+  setupController: function(controller, model) {
+	this.controllerFor('dashboard.organization.workspace.tasks.task').set('taskTypes', this.store.find('task-type'));
+	this._super(controller, model);
+  }
 });
