bb0d5b23f35af4807fdb9eb84c5cf72e0d8fbbee
Vitali Hatalski <v.hatalski@gmail.com>
2014-12-01 23:59:02 +0300

    these routes require authenticated session

--- ember/app/routes/dashboard/organization/workspace/tasks/rrule.js
+++ ember/app/routes/dashboard/organization/workspace/tasks/rrule.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
+import AuthenticatedRouteMixin from 'simple-auth/mixins/authenticated-route-mixin';
 
-export default Ember.Route.extend({
+export default Ember.Route.extend(AuthenticatedRouteMixin, {
 	model: function() {
 		var job = this.modelFor('dashboard.organization.workspace.tasks');
 		console.dir('model for rrule : ' + job);
