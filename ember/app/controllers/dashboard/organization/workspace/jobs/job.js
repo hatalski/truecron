afcfdae6297f42c4b5e1adb49002dd23492ffb84
Vitali Hatalski <v.hatalski@gmail.com>
2014-12-01 16:45:47 +0300

    comment schedule logs for now

--- ember/app/controllers/dashboard/organization/workspace/jobs/job.js
+++ ember/app/controllers/dashboard/organization/workspace/jobs/job.js
@@ -5,7 +5,7 @@ export default Ember.ObjectController.extend({
     repeatRules: ['Minutely', 'Hourly', 'Daily', 'Weekly', 'Monthly', 'Yearly'],
     selectedRepeatEvery: 1,
     repeatEvery: function() {
-        console.log('repeatEvery called');
+        // console.log('repeatEvery called');
         switch (this.get('selectedRepeatRule')) {
             case 'Minutely':
                 return [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60];
@@ -26,11 +26,11 @@ export default Ember.ObjectController.extend({
     recurrence: function() {
         var o = RRule.parseString(this.get('model.rrule'));
         var startsAt = this.get('model.startsAt');
-        console.log('startsAt : ' + startsAt);
+        // console.log('startsAt : ' + startsAt);
         if (startsAt === undefined) {
             startsAt = new Date();
         }
-        console.log('startsAt date : ' + new Date(startsAt));
+        // console.log('startsAt date : ' + new Date(startsAt));
         o.dtstart = new Date(startsAt);
         var rule = new RRule(o);
         var now = new Date();
