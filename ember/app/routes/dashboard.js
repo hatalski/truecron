4f02f9b1e4a56539ce0113aac6e073713ef8df13
Vitali Hatalski <v.hatalski@gmail.com>
2014-12-01 16:40:02 +0300

    redirect user to the first org / workspace

--- ember/app/routes/dashboard.js
+++ ember/app/routes/dashboard.js
@@ -5,8 +5,23 @@ export default Ember.Route.extend({
 		// TODO: replace 1 with authenticated user id
 		return this.store.find('person', 1);
 	},
-	afterModel: function() {
-		//console.dir('dashboard afterModel : ' + person.get('organizations').get('firstObject'));
-		//this.transitionTo('dashboard.organization', 'Personal');
+	afterModel: function(person) {
+		var self = this;
+		var length = person.get('organizations.length');
+		var organizations = person.get('organizations');
+		console.dir('redirect to organization if any exist : ' + length);
+		if (length > 0) {
+			var firstOrganization = organizations.get('firstObject');
+			self.store.find('organization', firstOrganization.get('id')).then(function(org) {
+				if (org.get('workspaces.length') > 0 ) {
+					var firstWorkspace = org.get('workspaces.firstObject');
+					self.store.find('workspace', firstWorkspace.get('id')).then(function(workspace) {
+						self.transitionTo('dashboard.organization.workspace.jobs', org, workspace);
+						self.controllerFor('dashboard').set('choosenOrganization', org);
+						self.controllerFor('dashboard').set('choosenWorkspace', workspace);
+					});
+				}
+		    });
+		}
 	}
 });
