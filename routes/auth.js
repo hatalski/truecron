1a58fddbb35fea1955aec82ca8ea6307cc9e4cb4
Vitali Hatalski <v.hatalski@gmail.com>
2014-12-17 01:53:59 +0300

    signup process updates

--- routes/auth.js
+++ routes/auth.js
@@ -6,8 +6,8 @@ var express = require('express'),
     validator = require('../lib/validator'),
     apiErrors = require('../lib/errors'),
     logger = require('../lib/logger'),
-    storage = require('../backend/storage');
-    //context = require('../context'),
+    storage = require('../backend/storage'),
+    context = require('../backend/context');
     //clientAuth = require('./clientauth'),
     //oauthErrors = require('./oautherrors'),
     //token = require('./token');
@@ -15,32 +15,48 @@ var express = require('express'),
 var router = express.Router();
 
 router.post('/signup', function(req, res, next) {
-    // check if there is an incoming parameter email and email is a valid email address
+    req.context = context.newSystemContext();
+
     var email = req.body.email;
-    // for some reason validator does not check email length,
-    // however according to the specs email cannot be more than 254 char length
-    // in our database max length is 256 however
-    // http://www.rfc-editor.org/errata_search.php?rfc=3696&eid=1690
+    var password = req.body.password;
     req.checkBody('email', 'The email address you entered is not valid. Please try again.').isEmail();
     req.checkBody('password', 'The password should be minimum of 8 characters in length.').isLength(8);
     var errors = req.validationErrors();
-    if (errors) {
-        return next(new apiErrors.InvalidParams(errors));
-    }
-    //if (!validator.isEmail(email) || email.length > 254) {
-    //    next(new apiErrors.InvalidParams('The email address you entered is not valid. Please try again.'));
-    //    //res.json(400, { error: { status: 400, message: "The email address you entered is not valid. Please try again." }});
-    //    //next();
-    //}
-    //1. Check if login (email) exists and if exists show message that email is already used in our system
+    if (errors) return next(new apiErrors.InvalidParams(errors));
+
+    var response = { organization: null, user: null, workspace: null };
+
     storage.Person.findByEmail(req.context, email)
-    .then(function(result) {
+        .then(function(result) {
+            // 1. Check if login (email) exists and if exists show message that email is already in use.
             if (result) {
                 return next(new apiErrors.InvalidParams('The email address is already taken. Please choose another one.'));
-                //res.json(400, { error: { status: 400, message: "The email address is already taken. Please choose another one." }});
-                //next();
             }
-            res.status(200).json({ message: "Your account has been created." });
+            // 2. Create user account
+            var attributes = { name: email, password: password};
+            console.dir(attributes);
+            return storage.Person.create(req.context, attributes);
+        })
+        .then(function (person) {
+            if (!person) logger.error('person was not created');
+            console.dir(person);
+            response.user = person;
+            req.context.personId = person.id;
+            // 3. Create "Personal" organization and add user as an admin of this organization
+            return storage.Organization.create(req.context, { name: 'Personal' });
+        })
+        .then(function (org) {
+            if (!org) logger.error('organization was not created');
+            console.dir(org);
+            response.organization = org;
+            //Add "My First" workspace and add user as an "editor"
+            return storage.Workspace.create(req.context, { name: 'My First'});
+        })
+        .then(function(wsp) {
+            if (!wsp) logger.error('workspace was not created');
+            console.dir(wsp);
+            response.workspace = wsp;
+            res.status(200).json(response);
         })
         .catch(function (err) {
             logger.error(err.toString());
