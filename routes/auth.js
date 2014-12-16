b9336753d3105f1ea883d8c3d08bb6bc266d97e7
Vitali Hatalski <v.hatalski@gmail.com>
2014-12-16 17:14:23 +0300

    sign up server method

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,59 @@
-var express = require('express');
-var path = require('path');
+var express = require('express'),
+    _ = require('lodash'),
+    util = require('util'),
+    Promise = require("bluebird"),
+    config = require('../lib/config'),
+    validator = require('../lib/validator'),
+    apiErrors = require('../lib/errors'),
+    logger = require('../lib/logger'),
+    storage = require('../backend/storage');
+    //context = require('../context'),
+    //clientAuth = require('./clientauth'),
+    //oauthErrors = require('./oautherrors'),
+    //token = require('./token');
+
 var router = express.Router();
+
+router.post('/signup', function(req, res, next) {
+    // check if there is an incoming parameter email and email is a valid email address
+    var email = req.body.email;
+    // for some reason validator does not check email length,
+    // however according to the specs email cannot be more than 254 char length
+    // in our database max length is 256 however
+    // http://www.rfc-editor.org/errata_search.php?rfc=3696&eid=1690
+    req.checkBody('email', 'The email address you entered is not valid. Please try again.').isEmail();
+    req.checkBody('password', 'The password should be minimum of 8 characters in length.').isLength(8);
+    var errors = req.validationErrors();
+    if (errors) {
+        return next(new apiErrors.InvalidParams(errors));
+    }
+    //if (!validator.isEmail(email) || email.length > 254) {
+    //    next(new apiErrors.InvalidParams('The email address you entered is not valid. Please try again.'));
+    //    //res.json(400, { error: { status: 400, message: "The email address you entered is not valid. Please try again." }});
+    //    //next();
+    //}
+    //1. Check if login (email) exists and if exists show message that email is already used in our system
+    storage.Person.findByEmail(req.context, email)
+    .then(function(result) {
+            if (result) {
+                return next(new apiErrors.InvalidParams('The email address is already taken. Please choose another one.'));
+                //res.json(400, { error: { status: 400, message: "The email address is already taken. Please choose another one." }});
+                //next();
+            }
+            res.status(200).json({ message: "Your account has been created." });
+        })
+        .catch(function (err) {
+            logger.error(err.toString());
+            return next(err);
+        });
+    //2. Create user account
+    //In case of SSO add necessary information to link Google (or other) account with account in our database
+    //3. Create "Personal" organization and add user as an admin of this organization
+    //Add "My First" workspace and add user as an "editor"
+    //4. Send welcome email with email verification code in it
+    //5. Authenticate user
+
+});
 
 router.get('/check', function(req, res) {
     if (req.isAuthenticated()) {
@@ -34,6 +87,19 @@ router.post('/simple.json', function(req, res) {
             message: 'Invalid login/password'
         });
     }
+});
+
+router.use(function(err, req, res, next) {
+    logger.error(util.inspect(err));
+    // Make sure the err has an appropriate status and a message
+    err = apiErrors.normalizeError(err);
+    res.status(err.status);
+    res.json({
+        error: {
+            status: err.status,
+            message: err.message
+        }
+    });
 });
 
 module.exports = router;
--- test/signup.js
+++ test/signup.js
@@ -10,7 +10,7 @@ prefix         = prefix || 'http://localhost:3000';
 
 log.info('BETA SING UP url prefix: ' + prefix);
 
-describe('SIGN UP',
+describe('BETA SIGN UP',
     function() {
         it('beta sign up', function (done) {
             superagent.post(prefix + '/beta/signup')
@@ -43,10 +43,99 @@ describe('SIGN UP',
                 .send({'email': '', 'test': true })
                 .end(function (e, res) {
                     expect(e).to.eql(null);
+                    expect(res.status).to.eql(400);
                     expect(res.header['content-type']).to.eql('application/json; charset=utf-8');
                     expect(res.body.error).to.eql(undefined);
                     //expect(res.body.message).to.eql('Incorrect Email!');
+                    done();
+                });
+        });
+    }
+);
+
+describe('REAL SIGN UP',
+    function() {
+        it('sign up should fail if email is invalid', function(done) {
+            superagent.post(prefix + '/auth/signup')
+                .send({'email': 'invalid', password: 'P@ssw0rd'})
+                .end(function (e, res) {
+                    expect(e).to.eql(null);
                     expect(res.status).to.eql(400);
+                    console.dir('body : ' + res.body);
+                    expect(res.header['content-type']).to.eql('application/json; charset=utf-8');
+                    expect(res.body.error).to.be.an('object');
+                    expect(res.body.error.status).to.eql(400);
+                    expect(res.body.error.message).to.eql('Invalid parameters. The email address you entered is not valid. Please try again.');
+                    done();
+                });
+        });
+
+        it('sign up should fail if email is empty', function(done) {
+            superagent.post(prefix + '/auth/signup')
+                .send({'email': '', password: 'P@ssw0rd'})
+                .end(function (e, res) {
+                    expect(e).to.eql(null);
+                    expect(res.status).to.eql(400);
+                    expect(res.header['content-type']).to.eql('application/json; charset=utf-8');
+                    expect(res.body.error).to.be.an('object');
+                    expect(res.body.error.status).to.eql(400);
+                    expect(res.body.error.message).to.eql('Invalid parameters. The email address you entered is not valid. Please try again.');
+                    done();
+                });
+        });
+
+        //it('sign up should fail if email length is invalid', function(done) {
+        //    superagent.post(prefix + '/auth/signup')
+        //        // 262 chars when 256 is max
+        //        .send({password: 'P@ssw0rd', 'email': '1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890@example.com'})
+        //        .end(function (e, res) {
+        //            expect(e).to.eql(null);
+        //            expect(res.status).to.eql(400);
+        //            expect(res.header['content-type']).to.eql('application/json; charset=utf-8');
+        //            expect(res.body.error).to.be.an('object');
+        //            expect(res.body.error.status).to.eql(400);
+        //            expect(res.body.error.message).to.eql('Invalid parameters. The email address you entered is not valid. Please try again.');
+        //            done();
+        //        });
+        //});
+
+        it('sign up should fail if email is already taken', function(done) {
+            superagent.post(prefix + '/auth/signup')
+                // 262 chars when 256 is max
+                .send({'email': 'bj@it.acme.corp', password: 'P@ssw0rd'})
+                .end(function (e, res) {
+                    expect(e).to.eql(null);
+                    expect(res.status).to.eql(400);
+                    expect(res.header['content-type']).to.eql('application/json; charset=utf-8');
+                    expect(res.body.error).to.be.an('object');
+                    expect(res.body.error.status).to.eql(400);
+                    expect(res.body.error.message).to.eql('Invalid parameters. The email address is already taken. Please choose another one.');
+                    done();
+                });
+        });
+
+        it('sign up should fail if password length is less than 8 characters', function(done) {
+            superagent.post(prefix + '/auth/signup')
+                .send({'email': 'vhatalski@naviam.com', password: 'P@ssw0r'})
+                .end(function (e, res) {
+                    expect(e).to.eql(null);
+                    expect(res.status).to.eql(400);
+                    expect(res.header['content-type']).to.eql('application/json; charset=utf-8');
+                    expect(res.body.error).to.be.an('object');
+                    expect(res.body.error.status).to.eql(400);
+                    expect(res.body.error.message).to.eql('Invalid parameters. The password should be minimum of 8 characters in length.');
+                    done();
+                });
+        });
+
+        it('sign up should success when email and password are valid', function(done) {
+            superagent.post(prefix + '/auth/signup')
+                .send({'email': 'vhatalski@naviam.com', password: 'P@ssw0rd'})
+                .end(function (e, res) {
+                    expect(e).to.eql(null);
+                    expect(res.status).to.eql(200);
+                    expect(res.header['content-type']).to.eql('application/json; charset=utf-8');
+                    expect(res.body.error).to.eql(undefined);
                     done();
                 });
         });
