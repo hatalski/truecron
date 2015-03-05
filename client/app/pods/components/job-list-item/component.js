ed5625d7ca8d3474cd7de5b1c6f1a6ffbcfb9a82
Vitali Hatalski <v.hatalski@gmail.com>
2015-03-05 12:19:17 +0300

    parsing rrule and adding data to the job list item template

--- client/app/pods/components/job-list-item/component.js
+++ client/app/pods/components/job-list-item/component.js
@@ -3,6 +3,41 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   tagName: 'a',
   classNames: ['list-group-item'],
+  attributeBindings: ['href'],
+  href: '#',
+  selectedRepeatRule: 'Daily',
+  repeatRules: ['Minutely', 'Hourly', 'Daily', 'Weekly', 'Monthly', 'Yearly'],
+  selectedRepeatEvery: 1,
+  repeatEvery: function() {
+    // console.log('repeatEvery called');
+    switch (this.get('selectedRepeatRule')) {
+      case 'Minutely':
+        return [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60];
+      case 'Hourly':
+        return [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24];
+      default:
+        return [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30];
+    }
+  }.property('selectedRepeatRule'),
+  running: false,
+  lastRunError: null,
+  recurrence: function() {
+    var o = RRule.parseString(this.get('job.rrule'));
+    var startsAt = this.get('job.startsAt');
+    // console.log('startsAt : ' + startsAt);
+    if (startsAt === undefined) {
+      startsAt = new Date();
+    }
+    // console.log('startsAt date : ' + new Date(startsAt));
+    o.dtstart = new Date(startsAt);
+    var rule = new RRule(o);
+    var now = new Date();
+    return {
+      text:    rule.toText(),
+      lastRun: rule.before(now, true),
+      nextRun: rule.after(now, true)
+    };
+  }.property('job.rrule', 'job.startsAt'),
   actions: {
     test: function() {
       "use strict";
