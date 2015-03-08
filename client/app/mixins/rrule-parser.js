c668021e3053b972139ba4934caeda14717a7280
Vitali Hatalski <v.hatalski@gmail.com>
2015-03-08 21:48:35 +0300

    add parsing and tests

--- client/app/mixins/rrule-parser.js
+++ client/app/mixins/rrule-parser.js
@@ -4,6 +4,62 @@ export default Ember.Mixin.create({
   //textStatement: '',
   recurrenceRuleToText: function(rrule) {
     "use strict";
+    var rules = rrule.split(';');
+    Ember.$.each(rules, function(index, rule) {
+      var options = rule.split('=');
+      var ruleName = options[0];
+      var ruleValue = options[1];
+      switch(ruleName) {
+        case 'FREQ':
+          switch(ruleValue) {
+            case 'SECONDLY':
+              break;
+            case 'MINUTELY':
+              break;
+            case 'HOURLY':
+              break;
+            case 'DAILY':
+              break;
+            case 'WEEKLY':
+              break;
+            case 'MONTHLY':
+              break;
+            case 'YEARLY':
+              break;
+          }
+          break;
+        case 'DTSTART':
+          break;
+        case 'UNTIL':
+          break;
+        case 'COUNT':
+          break;
+        case 'INTERVAL':
+          break;
+        case 'WKST':
+          break;
+        case 'BYDAY':
+          break;
+        case 'BYMONTH':
+          break;
+        case 'BYSETPOS':
+          break;
+        case 'BYMONTHDAY':
+          break;
+        case 'BYYEARDAY':
+          break;
+        case 'BYWEEKNO':
+          break;
+        case 'BYHOUR':
+          break;
+        case 'BYMINUTE':
+          break;
+        case 'BYSECOND':
+          break;
+        case 'BYEASTER':
+          break;
+      }
+    });
     return rrule;
   }
 });
--- client/tests/unit/mixins/rrule-parser-test.js
+++ client/tests/unit/mixins/rrule-parser-test.js
@@ -9,7 +9,30 @@ test('it works', function(assert) {
   var RruleParserObject = Ember.Object.extend(RruleParserMixin);
   var subject = RruleParserObject.create();
   assert.ok(subject);
-  debugger;
-  var text = subject.recurrenceRuleToText('FREQ=HOURLY;DTSTART=20150305T210000Z;INTERVAL=4;WKST=MO');
-  assert.same(text, 'Every 4 hours starting on 5 march 2015 at 21:00:00 GMT');
+});
+
+test('parsing hourly rrule interval', function() {
+  "use strict";
+  var RruleParserObject = Ember.Object.extend(RruleParserMixin);
+  var subject = RruleParserObject.create();
+
+  var text1 = subject.recurrenceRuleToText('FREQ=HOURLY;DTSTART=20150305T210000Z;INTERVAL=4;WKST=MO');
+  equal(text1, 'every 4 hours starting on 5 March 2015 at 21:00:00 GMT');
+  var text2 = subject.recurrenceRuleToText('FREQ=HOURLY;DTSTART=20150305T210000Z;INTERVAL=1;WKST=MO');
+  equal(text2, 'every hour starting on 5 March 2015 at 21:00:00 GMT');
+  var text3 = subject.recurrenceRuleToText('FREQ=HOURLY;DTSTART=20150305T210000Z;WKST=MO');
+  equal(text3, 'every hour starting on 5 March 2015 at 21:00:00 GMT');
+});
+
+test('parsing minutely rrule interval', function() {
+  "use strict";
+  var RruleParserObject = Ember.Object.extend(RruleParserMixin);
+  var subject = RruleParserObject.create();
+
+  var text1 = subject.recurrenceRuleToText('FREQ=MINUTELY;DTSTART=20150201T220500Z;INTERVAL=50;WKST=MO');
+  equal(text1, 'every 50 minutes starting on 1st February 2015 at 22:05:00 GMT');
+  var text2 = subject.recurrenceRuleToText('FREQ=MINUTELY;DTSTART=20150305T210000Z;INTERVAL=1;WKST=MO');
+  equal(text2, 'every minute starting on 5 March 2015 at 21:00:00 GMT');
+  var text3 = subject.recurrenceRuleToText('FREQ=MINUTELY;DTSTART=20150305T210000Z;WKST=MO');
+  equal(text3, 'every minute starting on 5 March 2015 at 21:00:00 GMT');
 });
