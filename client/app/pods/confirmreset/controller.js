66f82440e5297bd244c10fb413220c0c2f1b01ed
ghostxx7 <ghostxx7@gmail.com>
2015-03-12 13:24:45 +0300

    fix bug with controller

--- client/app/pods/confirmreset/controller.js
+++
@@ -1,9 +0,0 @@
-import Ember from 'ember';
-import ENV from 'true-cron/config/environment';
-import LoginControllerMixin from 'simple-auth/mixins/login-controller-mixin';
-
-
-export default Ember.Controller.extend({
-
-
-});
