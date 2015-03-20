7cd2fcec2d6572837964fed13686fd892d9d5c25
Vitali Hatalski <v.hatalski@gmail.com>
2015-03-20 12:02:18 +0300

    workspaces dropdown

--- client/app/pods/workspaces/controller.js
+++ client/app/pods/workspaces/controller.js
@@ -1,11 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  actions: {
-    changeWorkspace: function(workspace) {
-      "use strict";
-      Ember.Logger.log(workspace);
-      this.transitionToRoute('workspaces.workspace', workspace);
-    }
-  }
 });
