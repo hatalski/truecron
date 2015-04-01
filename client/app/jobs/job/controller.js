bb572529866e783dfe0f7b4f84741765ce19374b
Vitali Hatalski <v.hatalski@gmail.com>
2015-04-01 13:21:00 +0300

    new task form part 1

--- client/app/jobs/job/controller.js
+++ client/app/jobs/job/controller.js
@@ -2,10 +2,30 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   needs: ['jobs'],
-  jobs: Ember.computed.alias("controllers.jobs")//,
-  //selectJob: function() {
-  //  "use strict";
-  //  debugger;
-  //  this.get('jobs').selectJob(null);
-  //}
+  jobs: Ember.computed.alias("controllers.jobs"),
+  showNewTaskForm: false,
+  newTask: null,
+  actions: {
+    addTask: function() {
+      "use strict";
+      this.set('newTask', this.store.createRecord('task', {
+        name: 'new task'
+      }));
+      this.set('showNewTaskForm', true);
+    },
+    createTask: function(task) {
+      "use strict";
+      Ember.Logger.log('new task creation initiated');
+      Ember.Logger.log(task);
+      task.save();
+      this.set('newTask', null);
+      this.set('showNewTaskForm', false);
+    },
+    cancelTaskCreation: function() {
+      "use strict";
+      Ember.Logger.log('new task creation cancelled');
+      this.set('newTask', null);
+      this.set('showNewTaskForm', false);
+    }
+  }
 });
