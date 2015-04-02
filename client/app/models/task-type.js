628843e5ac8e93bab32c66bc16a5600cfebc7048
Vitali Hatalski <v.hatalski@gmail.com>
2015-04-02 16:41:41 +0300

    add task from ember

--- client/app/models/task-type.js
+++ client/app/models/task-type.js
@@ -1,8 +1,9 @@
 export default [
-	{ id: 0, name: 'empty' }, 
-	{ id: 1, name: 'file' }, 
-	{ id: 2, name: 'execute' }, 
-	{ id: 3, name: 'archive' }, 
-	{ id: 4, name: 'sftp' }, 
-	{ id: 5, name: 'smtp' }, 
+  { id: -1, name: 'default' },
+  { id: -1, name: 'default' },
+	{ id: 1, name: 'file' },
+	{ id: 2, name: 'execute' },
+	{ id: 3, name: 'archive' },
+	{ id: 4, name: 'sftp' },
+	{ id: 5, name: 'smtp' },
 	{ id: 6, name: 'http' }];
