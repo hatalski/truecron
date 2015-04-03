fd048399c1428d916ca26d3ec29de72754e76824
Vitali Hatalski <v.hatalski@gmail.com>
2015-04-04 00:03:17 +0300

    install nodejs 0.12.1 in vagrant

--- infrastructure/dev/provision/nodejs.sh
+++ infrastructure/dev/provision/nodejs.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
-add-apt-repository -y ppa:chris-lea/node.js
-apt-get update
-apt-get -y install nodejs
+
+# https://nodesource.com/blog/nodejs-v012-iojs-and-the-nodesource-linux-repositories
+# Note the new setup script name for Node.js v0.12
+curl -sL https://deb.nodesource.com/setup_0.12 | sudo bash -
+# Then install with:
+apt-get install -y nodejs
+
 npm install npm -g
 npm install -g supervisor
-
-#dependencies for hellocron app
-npm install -g minimist
 
 #end of file
