9c785dd2dd2fb348344b59378132799595719a22
Vitali Hatalski <v.hatalski@gmail.com>
2015-04-04 13:50:10 +0300

    remove unnecessary fields from job and task models

--- infrastructure/dev/provision/nodejs.sh
+++ infrastructure/dev/provision/nodejs.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-
 # https://nodesource.com/blog/nodejs-v012-iojs-and-the-nodesource-linux-repositories
 # Note the new setup script name for Node.js v0.12
 curl -sL https://deb.nodesource.com/setup_0.12 | sudo bash -
