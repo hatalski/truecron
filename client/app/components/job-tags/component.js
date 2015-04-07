5a3aae4872e6589b046e5e9d48b605a7aa1560ea
Vitali Hatalski <v.hatalski@gmail.com>
2015-04-07 21:17:52 +0300

    save job tags to db

--- client/app/components/job-tags/component.js
+++ client/app/components/job-tags/component.js
@@ -1,8 +1,29 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  initializeSelectize: function() {
+  tags: '',
+  didInsertElement: function() {
     "use strict";
+    var self = this;
+
     Ember.Logger.log('job-tags component has been inserted into the dom');
-  }.on('didInsertElement')
+
+    Ember.$('#jobTags').selectize({
+      delimiter: ',',
+      persist: false,
+      items: self.get('job.tags') ? [] : self.get('job.tags').toString(),
+      //render: {
+      //  item: function(data, escape) {
+      //    return '<span class="item label label-material-blue-grey-400">' + escape(data.value) + '</span>';
+      //  }
+      //},
+      create: function(input) {
+        self.sendAction('action', input);
+        return {
+          value: input,
+          text: input
+        };
+      }
+    });
+  }
 });
