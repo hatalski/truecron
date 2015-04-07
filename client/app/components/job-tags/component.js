f239ba27a4327a3089335a012ec55212bebd620b
Vitali Hatalski <v.hatalski@gmail.com>
2015-04-07 21:27:35 +0300

    fix selectize init

--- client/app/components/job-tags/component.js
+++ client/app/components/job-tags/component.js
@@ -7,11 +7,12 @@ export default Ember.Component.extend({
     var self = this;
 
     Ember.Logger.log('job-tags component has been inserted into the dom');
+    Ember.Logger.log('tags are: ', self.get('job.tags'));
 
     Ember.$('#jobTags').selectize({
       delimiter: ',',
       persist: false,
-      items: self.get('job.tags') ? [] : self.get('job.tags').toString(),
+      items: self.get('job.tags'),
       //render: {
       //  item: function(data, escape) {
       //    return '<span class="item label label-material-blue-grey-400">' + escape(data.value) + '</span>';
