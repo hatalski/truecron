32351265563baa030ef406e11d34fb0328c21d42
Vitali Hatalski <v.hatalski@gmail.com>
2015-04-15 15:48:25 +0300

    add sweet alert for prettier alerts

--- client/Brocfile.js
+++ client/Brocfile.js
@@ -49,6 +49,7 @@ app.import('bower_components/font-awesome/fonts/fontawesome-webfont.woff', { des
 app.import('bower_components/font-awesome/fonts/fontawesome-webfont.woff2', { destDir: "fonts"});
 app.import('bower_components/selectize/dist/css/selectize.css');
 app.import('bower_components/selectize/dist/css/selectize.bootstrap3.css');
+app.import('bower_components/sweetalert/lib/sweet-alert.css');
 
 // js
 app.import('bower_components/jquery/dist/jquery.js');
@@ -66,6 +67,7 @@ app.import('bower_components/rrule/lib/nlp.js');
 app.import('bower_components/moment/min/moment.min.js');
 app.import('bower_components/moment-timezone/builds/moment-timezone-with-data.js');
 app.import('bower_components/validator-js/validator.min.js');
+app.import('bower_components/sweetalert/lib/sweet-alert.js');
 
 // Use `app.import` to add additional libraries to the generated
 // output files.
