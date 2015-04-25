72d8dc0e3421d78683cc65d4b53715339e8d8bd8
Vitali Hatalski <v.hatalski@gmail.com>
2015-04-25 11:19:32 +0300

    fix mongodb bash script

--- infrastructure/dev/provision/mongodb.sh
+++ infrastructure/dev/provision/mongodb.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 
-sudo apt-get install mongodb
-sudo service mongodb start
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+echo "deb http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+
+#sudo apt-get install mongodb
+sudo service mongod start
+
+cat /var/log/mongodb/mongod.log
