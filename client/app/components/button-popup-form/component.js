99ed31a57d3b98135c03f5067a59c2d027f9529d
Vitali Hatalski <v.hatalski@gmail.com>
2015-04-30 11:59:58 +0300

    ability to add organizations and workspaces

---
+++ client/app/components/button-popup-form/component.js
@@ -0,0 +1,32 @@
+/* global swal */
+
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+    tagName: 'a',
+    classNames: ['btn', 'btn-signup', 'btn-material-orange-800'],
+    click: function() {
+        "use strict";
+        var self = this;
+        swal({
+            title: self.get('alertTitle'),
+            text: self.get('alertText'),
+            type: "input",
+            showCancelButton: true,
+            animation: "slide-from-top",
+            inputPlaceholder: "Please type name here",
+            closeOnConfirm: false
+        }, function(inputValue) {
+            if (inputValue === false) return false;
+            if (inputValue === "") {
+                swal.showInputError("This field is required.");
+                return false;
+            }
+            if(self.get('parent')) {
+                self.sendAction('action', inputValue, self.get('parent'));
+            } else {
+                self.sendAction('action', inputValue);
+            }
+        });
+    }
+});
--- client/app/workspaces/index/controller.js
+++ client/app/workspaces/index/controller.js
@@ -2,13 +2,26 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
     actions: {
-        addOrganization: function() {
+        addOrganization: function(newOrganizationName) {
             "use strict";
-
+            Ember.Logger.log('Organization should be added now.', newOrganizationName);
+            var organization = this.store.createRecord('organization', {
+                name: newOrganizationName
+            });
+            organization.save().then(function(newOrg) {
+                swal("Nice!", "Organization has been created with a name: " + newOrg.get('name'), "success");
+            });
         },
-        addWorkspace: function() {
+        addWorkspace: function(newWorkspaceName, organization) {
             "use strict";
-
+            Ember.Logger.log('Workspace should be added now.', newWorkspaceName);
+            var workspace = this.store.createRecord('workspace', {
+                name: newWorkspaceName,
+                organization: organization
+            });
+            workspace.save().then(function(newWsp) {
+                swal("Nice!", "Workspace has been created with a name: " + newWsp.get('name'), "success");
+            });
         }
     }
 });
