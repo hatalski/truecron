8e0124da1050c7074cbcf3fdb252f100f7c66b85
Vitali Hatalski <v.hatalski@gmail.com>
2015-04-30 10:44:09 +0300

    add org and workspace handlers

---
+++ client/app/workspaces/index/controller.js
@@ -0,0 +1,14 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+    actions: {
+        addOrganization: function() {
+            "use strict";
+
+        },
+        addWorkspace: function() {
+            "use strict";
+
+        }
+    }
+});
