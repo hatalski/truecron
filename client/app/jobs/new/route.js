e1d7c0315776746b15d632d2c61d3e57c620a800
Vitali Hatalski <v.hatalski@gmail.com>
2015-07-09 13:09:29 +0300

    fixed issue with updatedBy

--- client/app/jobs/new/route.js
+++ client/app/jobs/new/route.js
@@ -4,23 +4,33 @@ export default Ember.Route.extend({
   model: function() {
       "use strict";
       var workspace = this.modelFor('workspaces/workspace');
-      var userId = this.get('session.userId');
-      Ember.Logger.log('session user id: ', userId);
+      var user = this.controllerFor('jobs.new').get('user');
+      Ember.Logger.log('session user id: ', user);
+      Ember.Logger.log('session', this.get('session'));
       var newJob = this.store.createRecord('job', {
           name: '',
           workspace: workspace,
           workspaceId: workspace.get('id'),
-          updatedBy: userId
+          updatedBy: user
       }); //this.controllerFor('jobs').get('newJob');
       this.controllerFor('jobs').set('selectedJob', newJob);
       this.controllerFor('jobs').set('showJobDetails', true);
       Ember.Logger.log('new job on new route: ', newJob);
       return newJob;
   },
+    beforeModel: function(t) {
+        // set user for job controller
+        var jobController = this.controllerFor('jobs.new');
+        var userId = this.get('session.userId');
+        Ember.Logger.log('job after model userid', userId);
+        if (userId) {
+            jobController.set('user', this.store.find('user', userId));
+        }
+    },
   afterModel: function(model) {
-    "use strict";
-    if (!model) {
-      this.transitionTo('jobs');
-    }
+      "use strict";
+      if (!model) {
+          this.transitionTo('jobs');
+      }
   }
 });
